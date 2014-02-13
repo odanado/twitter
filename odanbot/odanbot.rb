514a94148d977a05a4a62da31bf4712396f963dd
odan <odan3240@gmail.com>
2014-02-13 13:26:15 +0900

    Fix start and stop message

--- odanbot/odanbot.rb
+++ odanbot/odanbot.rb
@@ -6,14 +6,16 @@ if ARGV.empty?
     exit
 end
 
+time = Time.now.to_s
+
 case ARGV[0]
 when "start"
-    system('./tweet.rb 起動しました.')
+    system('./tweet.rb 起動しました. ' + time)
     system('nohup ./mention_event.rb >> out.log 2>> err.log < /dev/null &')
 
 when "stop"
     system('killall mention_event.rb')
-    system('./tweet.rb 終了しました.')
+    system('./tweet.rb 終了しました. ' + time)
 
 when "restart"
     system('./odanbot.rb stop')
