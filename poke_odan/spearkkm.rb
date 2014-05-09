9a9e76998dd6cdb14e7e202beb6c67aec3286c4c
odan <odan3240@gmail.com>
2014-05-09 21:52:28 +0900

    Implement monitor spearkkm

--- poke_odan/spearkkm.rb
+++ poke_odan/spearkkm.rb
@@ -5,4 +5,12 @@ require 'twitter'
 
 load File.expand_path(File.dirname($0))+"/twitter_config.rb"
 
-p $rest_client.user("spearkkm")
+user_data = $rest_client.user("spearkkm")
+
+text = Time.now.to_s + ","
+text += user_data.name + ","
+text += user_data.description + ","
+text += user_data.location
+
+puts text
+
